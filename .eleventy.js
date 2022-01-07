32e395184b06bdeac658150ff6928653bb730c34
Oscar Palmér <opalmer@me.com>
2022-01-07 11:42:55 +0100

    11ty stuff
    - ~1.0 for 11ty
    - global data
    - plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,48 @@
-module.exports = function (config) {
-  config.setBrowserSyncConfig({
-    files: 'build/**/*',
-    port: 4567,
-    ui: false
-  });
+const path = require('path');
+
+const sass = require('eleventy-plugin-dart-sass');
+
+const mode = process.env.ELEVENTY_MODE || 'development';
+const production = mode === 'production';
+
+const browserOptions = {
+  files: 'build/**/*',
+  port: 4567,
+  ui: false
+};
+
+const htmlOptions = {
+  collapseWhitespace: true,
+  decodeEntities: true,
+  removeComments: true,
+};
+
+const sassOptions = {
+  domainName: 'https://oscarpalmer.se',
+  outDir: path.normalize(path.join(__dirname, './build')),
+  outFileName: 'styles',
+  outputStyle: production ? 'compressed' : 'expanded',
+  sassIndexFile: 'styles.scss',
+  sassLocation: path.normalize(path.join(__dirname, './source/assets/css/')),
+};
+
+module.exports = (config) => {
+  config.addGlobalData('production', production);
+  config.addGlobalData('timestamp', Date.now());
+
+  if (production) {
+    const html = require('html-minifier');
+
+    config.addTransform('html', (content, path) => {
+      return path.endsWith('.html')
+        ? html.minify(content, htmlOptions)
+        : content;
+    });
+  }
+
+  config.addPlugin(sass, sassOptions);
+
+  config.setBrowserSyncConfig(browserOptions);
 
   return {
     dir: {
@@ -13,6 +52,6 @@ module.exports = function (config) {
     },
     htmlTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk',
-    passthroughFileCopy: true
+    passthroughFileCopy: true,
   };
 };
